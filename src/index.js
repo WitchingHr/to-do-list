2f08e35378f22c79dcb27157b16337195ed95f2e
WitchingHr <matt1494@gmail.com>
2022-12-29 15:02:55 -0500

    Add more icons and animate sidebar exit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import css from './styles/index.css';
+
+const hamburger = document.querySelector('.hamburger');
+hamburger.addEventListener('click', toggleSidebar);
+
+const sidebar = document.querySelector('.sidebar');
+const tasks = document.querySelector('.tasks');
+
+function toggleSidebar() {
+  sidebar.classList.toggle('invisible');
+  tasks.classList.toggle('stretch');
+}
